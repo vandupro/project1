f6e5c2e94746e3cbeb0ecc76195f529b1c355362
unknown <dunvph10007@fpt.edu.vn>
2020-07-23 13:58:50 +0700

    modified js and html on 23/7

--- project1/index.js
+++ project1/index.js
@@ -1,24 +1,7 @@
-var x = $('ul.list a');
+var x = $('ul.list li');
 var y = $('.content');
 x.click(function(){
-	y.toggle(500);
+	
+	$(this).children('.content').toggle(500);
+	$(this).find('i').toggleClass('fa-plus');
 });
-// document.getElementsByTagName('a');
-// var content = document.getElementsByClassName('content');
-// var a = document.getElementsByTagName('a');
-// for(i = 0; i < a.length; i++){
-//     a[i].onclick = function(){
-//         // alert('abch')
-//         content[i].style.display = "";
-//     }
-//     break;
-// }
-// var x = $('ul.list a');
-// var y = $('.content');
-// for(i = 0; i < x.length; i++){
-//     x[i].click(function(){
-//         alert('abcch')
-//         y[i].toggle(400);
-//     })
-//     break;
-// }
